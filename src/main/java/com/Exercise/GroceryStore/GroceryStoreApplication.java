21a411141d603b2ce7136487c1853de9d5fb11fa
frodkata <fordkata@gmail.com>
2022-09-29 13:29:42 +0300

    Populate supported products on startup

--- src/main/java/com/Exercise/GroceryStore/GroceryStoreApplication.java
+++ src/main/java/com/Exercise/GroceryStore/GroceryStoreApplication.java
@@ -1,18 +1,24 @@
 package com.Exercise.GroceryStore;
 
 import com.Exercise.GroceryStore.Entities.Cart;
+import com.Exercise.GroceryStore.Entities.SupportedProducts;
 import com.Exercise.GroceryStore.Services.CartService;
+import com.Exercise.GroceryStore.Services.SupportedProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.servlet.mvc.condition.ProducesRequestCondition;
 
 @SpringBootApplication
 public class GroceryStoreApplication {
 
 	@Autowired
 	CartService cartService;
+
+	@Autowired
+	SupportedProductsService productsService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryStoreApplication.class, args);
@@ -21,9 +27,23 @@ public class GroceryStoreApplication {
 	//Instantiate and save new cart on startup
 	//Since there are no user entities and only one user, there is only one cart instance
 	@EventListener(ApplicationReadyEvent.class)
-	public void doSomethingAfterStartup() {
+	public void createUponStart() {
 		Cart cart = new Cart();
 		cartService.saveCart(cart);
+		//Fruits
+		productsService.saveProduct(new SupportedProducts("Fruit", "Apple"));
+		productsService.saveProduct(new SupportedProducts("Fruit", "Orange"));
+		productsService.saveProduct(new SupportedProducts("Fruit", "Mango"));
+		productsService.saveProduct(new SupportedProducts("Fruit", "Papaya"));
+
+		//Vegetable
+		productsService.saveProduct(new SupportedProducts("Vegetable", "Cucumber"));
+		productsService.saveProduct(new SupportedProducts("Vegetable", "Tomato"));
+		productsService.saveProduct(new SupportedProducts("Vegetable", "Potato"));
+		productsService.saveProduct(new SupportedProducts("Vegetable", "Onion"));
+
+
+
 	}
 
 }
