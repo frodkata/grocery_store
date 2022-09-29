0e0f93429e1149a24ba01256ba956ecb08ca36f3
frodkata <fordkata@gmail.com>
2022-09-29 18:52:46 +0300

    Upgrade promotion alg to work with multiple promoted items

--- src/main/java/com/Exercise/GroceryStore/Services/PromotionImpl.java
+++ src/main/java/com/Exercise/GroceryStore/Services/PromotionImpl.java
@@ -7,8 +7,8 @@ import com.Exercise.GroceryStore.Repositories.PromotionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+
 
 @Service
 public class PromotionImpl implements PromotionService {
@@ -23,7 +23,6 @@ public class PromotionImpl implements PromotionService {
     }
 
 
-
     @Override
     public Promotion savePromotion(Promotion promotion) {
         return promotionRepository.save(promotion);
@@ -32,7 +31,7 @@ public class PromotionImpl implements PromotionService {
     @Override
     public Promotion getPromotionById(Long id) {
         Optional<Promotion> optional = promotionRepository.findById(id);
-        Promotion promotion= null;
+        Promotion promotion = null;
         if (optional.isPresent()) {
             promotion = optional.get();
         }
@@ -52,8 +51,8 @@ public class PromotionImpl implements PromotionService {
 
     @Override
     public Promotion getPromotionByCategory(String categoryName) {
-        for (Promotion p: promotionRepository.findAll()) {
-            if(p.getPromotedCategory().toUpperCase().equals(categoryName.toUpperCase())){
+        for (Promotion p : promotionRepository.findAll()) {
+            if (p.getPromotedCategory().toUpperCase().equals(categoryName.toUpperCase())) {
                 return p;
             }
         }
@@ -63,8 +62,8 @@ public class PromotionImpl implements PromotionService {
 
     @Override
     public Promotion getPromotionByType(String promotionType) {
-        for (Promotion p: promotionRepository.findAll()) {
-            if(p.getPromotionType().toUpperCase().equals(promotionType.toUpperCase())){
+        for (Promotion p : promotionRepository.findAll()) {
+            if (p.getPromotionType().toUpperCase().equals(promotionType.toUpperCase())) {
                 return p;
             }
         }
@@ -75,8 +74,8 @@ public class PromotionImpl implements PromotionService {
 
     @Override
     public Boolean checkIfItemIsPromoted(String promotedItem, String promotionType) {
-        for (Promotion p: promotionRepository.findAll()) {
-            if(p.getPromotionType().equals(promotionType) && p.getPromotedItemNames().contains(promotedItem)){
+        for (Promotion p : promotionRepository.findAll()) {
+            if (p.getPromotionType().equals(promotionType) && p.getPromotedItemNames().contains(promotedItem)) {
                 return true;
             }
 
@@ -88,46 +87,51 @@ public class PromotionImpl implements PromotionService {
     @Override
     public double calculatePriceByPromotion(Cart cart) {
 
-       // String promoType = getPromotion().getPromotionType();
-       // String promoCategory = getPromotion().getPromotedCategory();
+
         double totalPrice = 0;
 
         //Get total price
-        for (Item i: cart.getItems()) {
-            totalPrice+=i.getItemPrice();
+        for (Item i : cart.getItems()) {
+            totalPrice += i.getItemPrice();
         }
 
-        for (Promotion promotion: promotionRepository.findAll()) {
+        for (Promotion promotion : promotionRepository.findAll()) {
+            //For case 2for3
             if (promotion.getPromotionType() != null && promotion.getPromotionType().equals("2for3") && cart.getItems().size() >= 2) {
-                
+
                 //Assume that the first item in the cart is the cheapest
                 double cheapestPrice = cart.getItems().get(0).getItemPrice();
 
                 for (int i = 0; i <= 2; i++) {
                     //Check if item is currently in promoted list and check if it's price is lower
-                    if(checkIfItemIsPromoted(cart.getItems().get(i).getName(), "2for3") && cart.getItems().get(i).getItemPrice() < cheapestPrice){
+                    if (checkIfItemIsPromoted(cart.getItems().get(i).getName(), "2for3") && cart.getItems().get(i).getItemPrice() < cheapestPrice) {
                         cheapestPrice = cart.getItems().get(i).getItemPrice();
                     }
                 }
 
-
                 totalPrice -= cheapestPrice;
             }
+            //For case buy1get1
             if (promotion.getPromotionType() != null && promotion.getPromotionType().equals(("buy1get1"))) {
-                int itemCount = 0;
-                for (Item item: cart.getItems()) {
-                    if(checkIfItemIsPromoted(item.getName(), "buy1get1")){
-                        itemCount++;
+
+                for (int i = 0; i < cart.getItems().size(); i++) {
+                    //Check if item is currently listed for promotion
+                    if (checkIfItemIsPromoted(cart.getItems().get(i).getName(), "buy1get1")) {
+                        //Loop trough rest of items to see if another item of same name exists
+                        for (int j = i + 1; j < cart.getItems().size(); j++) {
+                            //When a match is found, deduce the second item's price from the total price
+                            if(cart.getItems().get(i).getName().equals(cart.getItems().get(j).getName())){
+                               totalPrice -= cart.getItems().get(j).getItemPrice()*0.5;
+                               break;
+                            }
+                        }
                     }
 
-                    if(itemCount == 2){
-                        totalPrice -= item.getItemPrice()*0.50;
-                    }
 
                 }
             }
         }
-
         return totalPrice;
+
     }
 }
